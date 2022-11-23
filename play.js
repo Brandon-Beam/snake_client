f2ea737dbe2704fdb693747b3c3cc35d59d24711
Brandon <bbeam1@gmail.com>
2022-11-23 15:12:39 -0700

    refactored input

--- play.js
+++ play.js
@@ -1,18 +1,6 @@
 const net = require('net');
 const connect = require('./client')
-const setupInput = function () {
-  const stdin = process.stdin;
-  const handleUserInput = function (key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    stdin.on("data", handleUserInput);
-  };
-  return stdin;
-};
+const setupInput = require('./input')
 
 console.log('connecting...');
 connect();
