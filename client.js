0dd9562ce542cdf69e7e95351a1de82606ebf91c
Brandon <bbeam1@gmail.com>
2022-11-23 14:51:30 -0700

    more practice

--- client.js
+++ client.js
@@ -4,9 +4,13 @@ const connect = () => {
     host: '165.227.47.243',
     port: 50541,
   });
+  conn.on('connect', () => {
+    conn.write("Name: BB")
+    console.log('connection established')
+  })
   conn.on('data', (message) => {
     console.log({ message })
-  })
+  });
 
   conn.setEncoding('utf8')
   return conn;
