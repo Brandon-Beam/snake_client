465865c14ca7d4c636b450727c4c4597f1e45c08
Brandon <bbeam1@gmail.com>
2022-11-23 15:01:55 -0700

    start of ui

--- play.js
+++ play.js
@@ -1,4 +1,19 @@
 const net = require('net');
 const connect = require('./client')
+const setupInput = function () {
+  const stdin = process.stdin;
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", handleUserInput);
+  };
+  return stdin;
+};
+
 console.log('connecting...');
 connect();
+setupInput()
